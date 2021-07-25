529527135d6cb7cf35cb97764cb41acb2f40cd06
Luan Tran <luantranj@gmail.com>
2021-07-26 06:52:04 +0700

    Style rounded circle border radius for author image in ArticleCard

--- components/ArticleCard.js
+++ components/ArticleCard.js
@@ -7,13 +7,13 @@ export default function ArticleCard(props) {
 	return (<>
 		<div className="col-md-7 article pb-5 mb-5 border-bottom">
 			<h6><Link href={ "/" + catlink }><a className="text-decoration-none link-secondary">{ category }</a></Link></h6>
-			<h1>
-				<div className="display-6 fw-bold"><Link href={ "/" + slug}><a className="text-decoration-none">{ title }</a></Link></div>
-			</h1>
+			<h2 className="fw-bold">
+				<Link href={ "/" + slug}><a className="text-decoration-none">{ title }</a></Link>
+			</h2>
 			<div style={{ height: '15px', clear: 'both' }}></div>
 
 			<div className="d-flex align-items-center justify-content-center">
-				<span><Image src={ "/" + auimg } width="35" height="35" /></span>
+				<span><Image src={ "/" + auimg } width="35" height="35" className="rounded-circle" /></span>
 				<span className="px-5" className="ms-2" ><Link href={ "/" + aulink }><a className="text-decoration-none link-dark">{ author }</a></Link></span>
 				<span className="ms-4" >♦ { minread } min read</span>
 			</div> { /* article infor: end */ }
