258bc7eccda5d846c84c33bb3ea4b5e926a41570
Luan Tran <luantranj@gmail.com>
2021-07-28 09:43:48 +0700

    Finish 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,21 +1,18 @@
 import Link from 'next/link';
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+import BestArticles from '../components/BestArticles';
 
 export default function NotFound() {
-    const router = useRouter();
 
-    useEffect(() => {
-        setTimeout(() => {
-            router.push('/');
-        }, 4000);
-    }, []);
+	return (
+		<div className="not-found container-md fs-5 mt-5 pt-4">
+			<div className="text-center">
+				<h1 className="text-primary display-2 fw-bold">404</h1>
+				<p>The page you are looking for doesn't exist.</p>
+				<p>See the <Link href="/blog"><a className="text-decoration-none">blog</a></Link> for an overview of all articles.</p>
+				<p>Or try one of these article instead.</p>
+			</div> { /* 404 Notification: end */ }
 
-    return (
-        <div className="not-found">
-            <h1>404</h1>
-            <h2>Oooops! That page cannot be found</h2>
-            <p>Redirecting to the <Link href="/">Homepage</Link> for more running goodness...</p>
-        </div>
-    )
+			<BestArticles />
+		</div>
+)
 }
