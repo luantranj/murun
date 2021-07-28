fcffa6006db765d6cec556159dc56c7241a65b34
Luan Tran <luantranj@gmail.com>
2021-07-28 09:44:35 +0700

    Create Best Articles of the Blog component

---
+++ components/BestArticles.js
@@ -0,0 +1,115 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function BestArticles() {
+	return(<>
+		{ /* Best Articles of The Blog */ }
+		<div className="best-articles py-5 mt-5">
+			<div className="container p-6">
+				<div className="text-center">
+					<h2 className="fw-bold">Best of Empower Your Run Blog</h2>
+					<p className="fs-5">These few articles were handpicked from over 50 posts on my blog</p>
+				</div>
+
+
+				<div className="row justify-content-center justify-content-lg-between fs-5">
+					<div className="col-10 col-md-4 col-lg-3">
+						<div style={{ minHeight: '50px', clear: 'both'  }}></div>
+						<h4 className="fw-bold">Flat Feet</h4>
+						<ul className="list-unstyled d-grid gap-4">
+							<li className="position-relative">
+								<span style={{ position: 'absolute', top: 2, left: -24  }}><Image src="/star.png" width="19px" height="19px" alt="star" /></span>
+								<Link href="/flat-feet-exercises">
+									<a className="text-decoration-none">Flat Feet Exercises – The Ultimate Exercises To Fix Flat Feet!</a>
+								</Link>
+							</li>
+							<li><Link href="/flat-feet-exercises"><a className="text-decoration-none">Flat Feet Exercises – The Ultimate Exercises To Fix Flat Feet!</a></Link></li>
+							<li><Link href="/flat-feet-exercises"><a className="text-decoration-none">Flat Feet Exercises – The Ultimate Exercises To Fix Flat Feet!</a></Link></li>
+							<li><Link href="/flat-feet-exercises"><a className="text-decoration-none">Flat Feet Exercises – The Ultimate Exercises To Fix Flat Feet!</a></Link></li>
+						</ul>
+					</div>
+
+					<div className="col-10 col-md-4 col-lg-3">
+						<div style={{ minHeight: '50px', clear: 'both'  }}></div>
+						<h4 className="fw-bold">Flat Feet</h4>
+						<ul className="list-unstyled d-grid gap-4">
+							<li className="position-relative">
+								<span style={{ position: 'absolute', top: 2, left: -24  }}><Image src="/star.png" width="19px" height="19px" alt="star" /></span>
+								<Link href="/flat-feet-exercises">
+									<a className="text-decoration-none">Flat Feet Exercises – The Ultimate Exercises To Fix Flat Feet!</a>
+								</Link>
+							</li>
+							<li><Link href="/flat-feet-exercises"><a className="text-decoration-none">Flat Feet Exercises – The Ultimate Exercises To Fix Flat Feet!</a></Link></li>
+							<li><Link href="/flat-feet-exercises"><a className="text-decoration-none">Flat Feet Exercises – The Ultimate Exercises To Fix Flat Feet!</a></Link></li>
+							<li><Link href="/flat-feet-exercises"><a className="text-decoration-none">Flat Feet Exercises – The Ultimate Exercises To Fix Flat Feet!</a></Link></li>
+							<li><Link href="/flat-feet-exercises"><a className="text-decoration-none">Flat Feet Exercises – The Ultimate Exercises To Fix Flat Feet!</a></Link></li>
+						</ul>
+					</div>
+
+					<div className="col-10 col-md-4 col-lg-3">
+						<div style={{ minHeight: '50px', clear: 'both'  }}></div>
+						<h4 className="fw-bold">Flat Feet</h4>
+						<ul className="list-unstyled d-grid gap-4">
+							<li><Link href="/flat-feet-exercises"><a className="text-decoration-none">Flat Feet Exercises – The Ultimate Exercises To Fix Flat Feet!</a></Link></li>
+							<li className="position-relative">
+								<span style={{ position: 'absolute', top: 2, left: -24  }}><Image src="/star.png" width="19px" height="19px" alt="star" /></span>
+								<Link href="/flat-feet-exercises">
+									<a className="text-decoration-none">Flat Feet Exercises – The Ultimate Exercises To Fix Flat Feet!</a>
+								</Link>
+							</li>
+							<li><Link href="/flat-feet-exercises"><a className="text-decoration-none">Flat Feet Exercises – The Ultimate Exercises To Fix Flat Feet!</a></Link></li>
+							<li><Link href="/flat-feet-exercises"><a className="text-decoration-none">Flat Feet Exercises – The Ultimate Exercises To Fix Flat Feet!</a></Link></li>
+							<li><Link href="/flat-feet-exercises"><a className="text-decoration-none">Flat Feet Exercises – The Ultimate Exercises To Fix Flat Feet!</a></Link></li>
+							<li><Link href="/flat-feet-exercises"><a className="text-decoration-none">Flat Feet Exercises – The Ultimate Exercises To Fix Flat Feet!</a></Link></li>
+						</ul>
+					</div>
+
+					<div className="w-100"></div>
+
+					<div className="col-10 col-md-4 col-lg-3">
+						<div style={{ minHeight: '50px', clear: 'both'  }}></div>
+						<h4 className="fw-bold">Flat Feet</h4>
+						<ul className="list-unstyled d-grid gap-4">
+							<li><Link href="/flat-feet-exercises"><a className="text-decoration-none">Flat Feet Exercises – The Ultimate Exercises To Fix Flat Feet!</a></Link></li>
+							<li><Link href="/flat-feet-exercises"><a className="text-decoration-none">Flat Feet Exercises – The Ultimate Exercises To Fix Flat Feet!</a></Link></li>
+						</ul>
+					</div>
+
+					<div className="col-10 col-md-4 col-lg-3">
+						<div style={{ minHeight: '50px', clear: 'both'  }}></div>
+						<h4 className="fw-bold">Flat Feet</h4>
+						<ul className="list-unstyled d-grid gap-4">
+							<li className="position-relative">
+								<span style={{ position: 'absolute', top: 2, left: -24  }}><Image src="/star.png" width="19px" height="19px" alt="star" /></span>
+								<Link href="/flat-feet-exercises">
+									<a className="text-decoration-none">Flat Feet Exercises – The Ultimate Exercises To Fix Flat Feet!</a>
+								</Link>
+							</li>
+							<li><Link href="/flat-feet-exercises"><a className="text-decoration-none">Flat Feet Exercises – The Ultimate Exercises To Fix Flat Feet!</a></Link></li>
+							<li><Link href="/flat-feet-exercises"><a className="text-decoration-none">Flat Feet Exercises – The Ultimate Exercises To Fix Flat Feet!</a></Link></li>
+							<li><Link href="/flat-feet-exercises"><a className="text-decoration-none">Flat Feet Exercises – The Ultimate Exercises To Fix Flat Feet!</a></Link></li>
+						</ul>
+					</div>
+
+					<div className="col-10 col-md-4 col-lg-3">
+						<div style={{ minHeight: '50px', clear: 'both'  }}></div>
+						<h4 className="fw-bold">Flat Feet</h4>
+						<ul className="list-unstyled d-grid gap-4">
+							<li className="position-relative">
+								<span style={{ position: 'absolute', top: 2, left: -24  }}><Image src="/star.png" width="19px" height="19px" alt="star" /></span>
+								<Link href="/flat-feet-exercises">
+									<a className="text-decoration-none">Flat Feet Exercises – The Ultimate Exercises To Fix Flat Feet!</a>
+								</Link>
+							</li>
+							<li><Link href="/flat-feet-exercises"><a className="text-decoration-none">Flat Feet Exercises – The Ultimate Exercises To Fix Flat Feet!</a></Link></li>
+							<li><Link href="/flat-feet-exercises"><a className="text-decoration-none">Flat Feet Exercises – The Ultimate Exercises To Fix Flat Feet!</a></Link></li>
+						</ul>
+					</div>
+	
+		
+
+				</div> { /* row: end */ }
+			</div> { /* container: end */}
+		</div> { /* Best Articles of The Blog: end */ }
+	</>)
+}
