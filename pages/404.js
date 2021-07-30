75d9f2f86b55177f43a212229209422e67c3540e
Luan Tran <luantranj@gmail.com>
2021-07-30 07:27:47 +0700

    Fix mispell article to article(s)

--- pages/404.js
+++ pages/404.js
@@ -9,7 +9,7 @@ export default function NotFound() {
 				<h1 className="text-primary display-2 fw-bold">404</h1>
 				<p>The page you are looking for doesn't exist.</p>
 				<p>See the <Link href="/blog"><a className="text-decoration-none">blog</a></Link> for an overview of all articles.</p>
-				<p>Or try one of these article instead.</p>
+				<p>Or try one of these articles instead.</p>
 			</div> { /* 404 Notification: end */ }
 
 			<BestArticles />
