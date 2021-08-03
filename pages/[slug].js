2ca245488f8e3ae3a6b4fe8d51f25794cc184e63
Luan Tran <luantranj@gmail.com>
2021-08-03 11:43:58 +0700

    Start build Article page for each article

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,3 +1,8 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+
+
 export async function getStaticPaths() {
 	const res = await fetch(process.env.ARTICLES_ENDPOINT);
 	const data = await res.json();
@@ -43,14 +48,46 @@ export async function getStaticProps(context) {
 
 
 export default function Article({ infoData, data }) {
-	return (
-		<div>
-			<h1>{ infoData.title }</h1>	
-			<span>{ infoData.author_avatar }</span>
-			<span>{ infoData.author_name }</span>
-			<span>{ infoData.min_read }</span>
-			<span>{ infoData.topic }</span>
-			<p>{ infoData.meta_description }</p>
-		</div>
-	)
+	const topicSlugs = ['training', 'injuries', 'running-equipment', 'frequently-asked-questions'];
+	const topics = ['TRAINING', 'INJURIES', 'RUNNING EQUIPMENT', 'FAQs'];
+	const formattedFeaturedImageLink = infoData.feature_image.split('^'); 
+
+	return (<>
+		<Head>
+			<title>{ infoData.title }</title>
+			<meta name="description" content={ infoData.meta_description } />
+		</Head>
+
+		{ /* Article Info */ }
+		<div className="artilce-info py-4 bg-primary">
+			<div className="container-lg text-center text-light">
+				<div className="row justify-content-center">
+					<div className="col-md-8">
+						<Image className='article-image' src={ formattedFeaturedImageLink[0] } width={ formattedFeaturedImageLink[1] } height={ formattedFeaturedImageLink[2] } alt={ infoData.title } layout='intrinsic' />
+						<p className="mt-2"><Link href={ '/category/' +  topicSlugs[infoData.topic] }><a className="text-decoration-none link-light fw-bold">{ topics[infoData.topic] }</a></Link></p>
+						<h1 className="fw-bold">{ infoData.title }</h1>	
+						<div className="mt-4 mb-md-4">
+							<Link href={ infoData.author_link }><a className="text-decoration-none link-light">{ infoData.author_name }</a></Link>
+							<span className="px-3">&#9642;</span>
+							<span>{ infoData.min_read } min read</span>
+						</div>
+					</div> { /* col: end */ }
+				</div> { /* row: end */ }
+			</div> { /* container: end */ }
+		</div>{ /* Article Info: end */ }
+
+
+		{ /* Article Content */ }
+		<div className="artilce-content mt-4 py-4 fs-5">
+			<div className="container-lg ">
+				<div className="row justify-content-center">
+					<div className="col-md-8 col-lg-6">
+						{data.content.map((line, index) =>
+							<span key={ index } dangerouslySetInnerHTML={{ __html: line }}></span>
+						)}
+					</div> { /* col: end */ }
+				</div> { /* row: end */ }
+			</div> { /* container: end */ }
+		</div>{ /* Article Content: end */ }
+	</>)
 }
