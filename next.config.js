8a4bdb77abd82c8f87cc7075e58286fbe1ce8d0a
Luan Tran <luantranj@gmail.com>
2021-08-08 10:17:17 +0700

    Add picsum.photos to whitelist

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 // next.config.js
 module.exports = {
   images: {
-    domains: ['images.ctfassets.net', 'res.cloudinary.com'],
+    domains: ['images.ctfassets.net', 'res.cloudinary.com', 'm.media-amazon.com', 'picsum.photos'],
   },
 }
